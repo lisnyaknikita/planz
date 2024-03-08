8ea76e85d4a39d9c1d63801bbc8fb5cbae2be354
lisnyaknikita <lisnyaknikita@gmail.com>
2024-03-08 22:54:12 +0200

    feat: created layout file

---
+++ src/ui/layout/Layout.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import classes from './Layout.module.scss'
+
+const Layout: FC = () => {
+	return <div className={classes.layout}>Component</div>
+}
+
+export default Layout
