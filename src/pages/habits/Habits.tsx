5dced4e18d9ceb28dbcf31556e78eebb1b44c96e
nlisnyak <lisnyaknikita@gmail.com>
2024-09-29 15:00:03 +0300

    fix: fixed project card, implemented random colors and deleting projects

--- src/pages/habits/Habits.tsx
+++ src/pages/habits/Habits.tsx
@@ -20,167 +20,6 @@ import plusButton from '../../assets/icons/plus.svg'
 import Modal from '../../ui/modal/Modal'
 import { Habit } from '../projects/types/types'
 
-// const HabitsPage: FC = () => {
-// 	const [habits, setHabits] = useState<Habit[]>([])
-// 	const [isHabitModalOpened, setIsHabitModalOpened] = useState(false)
-// 	const [newHabitTitle, setNewHabitTitle] = useState<string>('')
-// 	const [error, setError] = useState<string>('')
-// 	const { currentUser } = auth
-
-// 	const habitsCollectionRef = collection(db, 'habits')
-
-// 	const firestore = getFirestore()
-
-// 	function toggleModalVisibility() {
-// 		setIsHabitModalOpened(true)
-// 	}
-
-// 	const fetchHabits = async () => {
-// 		try {
-// 			const q = query(
-// 				habitsCollectionRef,
-// 				where('userId', '==', currentUser?.uid)
-// 			)
-// 			const data = await getDocs(q)
-
-// 			const filteredData = data.docs.map(doc => ({
-// 				...doc.data(),
-// 				id: doc.id,
-// 			}))
-// 			setHabits(filteredData)
-// 		} catch (error) {
-// 			console.error(error)
-// 		}
-// 	}
-
-// 	useEffect(() => {
-// 		fetchHabits()
-// 		document.title = 'Planz | Habits'
-// 	}, [])
-
-// 	const onSubmitHabit = async (e: React.FormEvent<HTMLFormElement>) => {
-// 		e.preventDefault()
-
-// 		if (!newHabitTitle.trim()) {
-// 			setError('Title cannot be empty')
-// 			return
-// 		}
-
-// 		try {
-// 			await addDoc(collection(db, 'habits'), {
-// 				title: newHabitTitle,
-// 				completed: false,
-// 				userId: auth?.currentUser?.uid,
-// 			})
-// 			setNewHabitTitle('')
-// 			setError('')
-// 			setIsHabitModalOpened(false)
-// 			fetchHabits()
-// 		} catch (error) {
-// 			setError('Failed to create habit')
-// 			console.error('Error creating habit:', error)
-// 		}
-// 	}
-
-// 	const deleteHabit = async (id: string) => {
-// 		if (confirm('Do you really want to delete this habit&')) {
-// 			try {
-// 				await deleteDoc(doc(firestore, 'habits', id))
-// 				setHabits(habits.filter(habit => habit.id !== id))
-// 			} catch (error) {
-// 				console.error('Error deleting habit: ', error)
-// 			}
-// 		}
-// 	}
-
-// 	const toggleHabitStatus = async (id: string) => {
-// 		const updatedHabits = habits.map(habit =>
-// 			habit.id === id ? { ...habit, completed: !habit.completed } : habit
-// 		)
-// 		setHabits(updatedHabits)
-// 		try {
-// 			await updateDoc(doc(firestore, 'habits', id), {
-// 				completed: !habits.find(habit => habit.id === id)?.completed,
-// 			})
-// 		} catch (error) {
-// 			console.error('Error updating habit: ', error)
-// 		}
-// 	}
-
-// 	return (
-// 		<>
-// 			<div className={classes.wrapper}>
-// 				<button
-// 					className={classes.addHabitButton}
-// 					onClick={toggleModalVisibility}
-// 				>
-// 					<img src={plusButton} alt='add new habit' />
-// 				</button>
-// 				<div className={classes.inner}>
-// 					<ul className={classes.habitsList}>
-// 						{habits.map(habit => (
-// 							<li className={classes.habit} key={habit.id}>
-// 								<p className={classes.habitName}>{habit.title}</p>
-// 								{habit.completed ? (
-// 									<button
-// 										className={classes.completedSpan}
-// 										onClick={() => toggleHabitStatus(habit.id)}
-// 									>
-// 										Completed
-// 									</button>
-// 								) : (
-// 									<button
-// 										className={classes.completeButton}
-// 										onClick={() => toggleHabitStatus(habit.id)}
-// 									>
-// 										<img src={completeButton} alt='complete button' />
-// 									</button>
-// 								)}
-
-// 								<button
-// 									className={classes.deleteButton}
-// 									onClick={() => deleteHabit(habit.id)}
-// 								>
-// 									<img src={deleteButton} alt='delete this habit' />
-// 								</button>
-// 							</li>
-// 						))}
-// 					</ul>
-// 				</div>
-// 			</div>
-// 			{isHabitModalOpened && (
-// 				<Modal
-// 					setIsHabitModalOpened={setIsHabitModalOpened}
-// 					isHabitModalOpened={isHabitModalOpened}
-// 				>
-// 					<div className={classes.modalBody} onClick={e => e.stopPropagation()}>
-// 						<form
-// 							className={classes.createNewHabitForm}
-// 							onSubmit={onSubmitHabit}
-// 						>
-// 							<input
-// 								className={classes.newHabitName}
-// 								type='text'
-// 								placeholder='New habit...'
-// 								value={newHabitTitle}
-// 								onChange={e => setNewHabitTitle(e.target.value)}
-// 								required
-// 								autoFocus
-// 							/>
-// 							{error && <p className={classes.error}>{error}</p>}
-// 							<button className={classes.createButton} type='submit'>
-// 								Create new habit
-// 							</button>
-// 						</form>
-// 					</div>
-// 				</Modal>
-// 			)}
-// 		</>
-// 	)
-// }
-
-// export default HabitsPage
-
 const HabitsPage: FC = () => {
 	const [habits, setHabits] = useState<Habit[]>([])
 	const [isHabitModalOpened, setIsHabitModalOpened] = useState(false)
@@ -191,20 +30,16 @@ const HabitsPage: FC = () => {
 	const habitsCollectionRef = collection(db, 'habits')
 	const firestore = getFirestore()
 
-	// Функция для сброса статусов привычек
-	// Функция для сброса статусов привычек
 	const resetHabitStatuses = async () => {
 		try {
 			const snapshot = await getDocs(habitsCollectionRef)
 
-			// Обновляем каждую привычку по отдельности
 			const updates = snapshot.docs.map(async doc => {
 				await updateDoc(doc.ref, { completed: false })
 			})
 
-			await Promise.all(updates) // Ждем завершения всех обновлений
-			fetchHabits() // Обновляем список привычек
-			// Обновляем время последнего сброса
+			await Promise.all(updates)
+			fetchHabits()
 			localStorage.setItem('lastReset', Date.now().toString())
 		} catch (error) {
 			console.error('Error resetting habit statuses: ', error)
@@ -244,9 +79,7 @@ const HabitsPage: FC = () => {
 			0
 		)
 
-		// Проверяем, если последний сброс был до установленного времени сброса
 		if (!lastReset || new Date(Number(lastReset)) < resetTime) {
-			// Если текущее время меньше, чем время сброса, не вызываем сброс
 			if (now >= resetTime) {
 				resetHabitStatuses()
 			}
--- src/pages/projects/components/projects-list/ProjectsList.tsx
+++ src/pages/projects/components/projects-list/ProjectsList.tsx
@@ -1,16 +1,64 @@
 import { FC, useEffect, useState } from 'react'
 
+import deleteButton from '../../../../assets/icons/delete.svg'
 import classes from './ProjectsList.module.scss'
 
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import {
+	collection,
+	deleteDoc,
+	doc,
+	getDocs,
+	query,
+	where,
+} from 'firebase/firestore'
 import { Link } from 'react-router-dom'
 import { auth, db } from '../../../../../firebaseConfig'
 
 const ProjectsList: FC = () => {
 	const { currentUser } = auth
-	const [projectList, setProjectList] = useState([])
+	const [projectList, setProjectList] = useState<
+		{ id: string; title: string; description: string; color: string }[]
+	>([])
+
+	const colorsArray = [
+		'#D65780',
+		'#C7A27C',
+		'#261447',
+		'#646E68',
+		'#B48EAE',
+		'#1D8A99',
+		'#C6B89E',
+		'#230903',
+		'#4B4237',
+		'#D5A021',
+		'#736B60',
+		'#7C6A0A',
+		'#EB6424',
+		'#4381C1',
+		'#4E4B5C',
+	]
+
+	const getRandomColor = (colorsArray: string[]) => {
+		const randomIndex = Math.floor(Math.random() * colorsArray.length)
+		return colorsArray[randomIndex]
+	}
 
 	const projectsCollectionRef = collection(db, 'projects')
+
+	const deleteProject = async (projectId: string) => {
+		const confirmDelete = confirm('Do you really want to delete this project?')
+		if (!confirmDelete) return
+
+		try {
+			await deleteDoc(doc(db, 'projects', projectId))
+
+			setProjectList(prevProjects =>
+				prevProjects.filter(project => project.id !== projectId)
+			)
+		} catch (error) {
+			console.error('Error deleting project: ', error)
+		}
+	}
 
 	useEffect(() => {
 		const getProjectList = async () => {
@@ -24,8 +72,8 @@ const ProjectsList: FC = () => {
 				const filteredData = data.docs.map(doc => ({
 					...doc.data(),
 					id: doc.id,
+					color: getRandomColor(colorsArray),
 				}))
-				console.log(filteredData)
 				setProjectList(filteredData)
 			} catch (error) {
 				console.error(error)
@@ -35,18 +83,23 @@ const ProjectsList: FC = () => {
 		getProjectList()
 	}, [currentUser])
 
-	console.log(projectList)
-
 	return (
 		<ul className={classes.projectsList}>
 			{projectList.map(project => (
-				<li className={classes.projectCard} key={project.id}>
-					{/* <button className={classes.deleteProjectButton}>
+				<li
+					className={classes.projectCard}
+					key={project.id}
+					style={{ backgroundColor: project.color }}
+				>
+					<button
+						className={classes.deleteProjectButton}
+						onClick={() => deleteProject(project.id)}
+					>
 						<img src={deleteButton} alt='delete project' />
-					</button> */}
-					<div className={classes.progress}>
+					</button>
+					{/* <div className={classes.progress}>
 						<span className={classes.percent}>25%</span>
-					</div>
+					</div> */}
 					<Link to={`/project/${project.id}`} className={classes.projectName}>
 						<h4>{project.title}</h4>
 					</Link>
