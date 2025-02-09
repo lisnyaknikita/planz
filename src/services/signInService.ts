6e80ee1058db5e95c04182f094a8dba8e6d77fa5
nlisnyak <lisnyaknikita@gmail.com>
2025-02-09 17:14:21 +0200

    fix: fixed unused variable

--- src/services/signInService.ts
+++ src/services/signInService.ts
@@ -3,8 +3,7 @@ import { auth } from '../../firebaseConfig.ts'
 
 const loginUser = async (email: string, password: string): Promise<void> => {
 	try {
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const userCredential = await signInWithEmailAndPassword(auth, email, password)
+		await signInWithEmailAndPassword(auth, email, password)
 	} catch (error) {
 		console.error('Error logging in user:', error)
 		throw error
