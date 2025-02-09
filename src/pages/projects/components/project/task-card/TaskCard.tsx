e8dd3e6e3d8ec08a60fc97a6d350a87ab7a235cd
nlisnyak <lisnyaknikita@gmail.com>
2025-02-09 16:01:40 +0200

    refactor: created useTaskActions custom hook

--- src/pages/projects/components/project/task-card/TaskCard.tsx
+++ src/pages/projects/components/project/task-card/TaskCard.tsx
@@ -1,13 +1,12 @@
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, useEffect } from 'react'
 
 import { ID, Task } from '../../../types/types'
 import classes from './TaskCard.module.scss'
 
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { doc, updateDoc } from 'firebase/firestore'
-import { db } from '../../../../../../firebaseConfig'
 import deleteTaskButton from '../../../../../assets/icons/delete.svg'
+import { useTaskActions } from '../../../../../hooks/projects/kanban/useTaskActions'
 
 interface ITaskCardProps {
 	task: Task
@@ -16,28 +15,16 @@ interface ITaskCardProps {
 }
 
 const TaskCard: FC<ITaskCardProps> = ({ task, deleteTask, updateTask }) => {
-	const [editMode, setEditMode] = useState(false)
-	const [priority, setPriority] = useState<string>(
-		task.priority || 'A1. High priority'
-	)
+	const { editMode, setEditMode, priority, handlePriorityChange, textAreaRef } = useTaskActions(task)
 
-	const textAreaRef = useRef<HTMLTextAreaElement>(null)
-
-	const priorities = [
-		'A1. High priority',
-		'A2. Medium priority',
-		'A3. Low priority',
-	]
-
-	const { setNodeRef, attributes, listeners, transform, transition } =
-		useSortable({
-			id: task.id,
-			data: {
-				type: 'Task',
-				task,
-			},
-			disabled: editMode,
-		})
+	const { setNodeRef, attributes, listeners, transform, transition } = useSortable({
+		id: task.id,
+		data: {
+			type: 'Task',
+			task,
+		},
+		disabled: editMode,
+	})
 
 	const style = {
 		transition,
@@ -50,40 +37,17 @@ const TaskCard: FC<ITaskCardProps> = ({ task, deleteTask, updateTask }) => {
 		'A3. Low priority': '#03CEA4',
 	}
 
-	const handlePriorityChange = () => {
-		const currentPriorityIndex = priorities.indexOf(priority)
-		const nextPriority =
-			priorities[(currentPriorityIndex + 1) % priorities.length]
-		setPriority(nextPriority)
-		updateTaskPriority(task.id, nextPriority)
-	}
-
-	async function updateTaskPriority(id: ID, newPriority: string) {
-		try {
-			const taskRef = doc(db, 'tasks', id.toString())
-			await updateDoc(taskRef, { priority: newPriority })
-		} catch (error) {
-			console.error('Error updating task priority: ', error)
-		}
-	}
-
 	useEffect(() => {
 		if (editMode && textAreaRef.current) {
 			const textArea = textAreaRef.current
 			textArea.focus()
 			textArea.setSelectionRange(textArea.value.length, textArea.value.length)
 		}
-	}, [editMode])
+	}, [editMode, textAreaRef])
 
 	if (editMode) {
 		return (
-			<div
-				className={classes.task}
-				ref={setNodeRef}
-				style={style}
-				{...attributes}
-				{...listeners}
-			>
+			<div className={classes.task} ref={setNodeRef} style={style} {...attributes} {...listeners}>
 				<div
 					className={classes.priority}
 					onClick={handlePriorityChange}
@@ -109,13 +73,7 @@ const TaskCard: FC<ITaskCardProps> = ({ task, deleteTask, updateTask }) => {
 	}
 
 	return (
-		<div
-			className={classes.task}
-			ref={setNodeRef}
-			style={style}
-			{...attributes}
-			{...listeners}
-		>
+		<div className={classes.task} ref={setNodeRef} style={style} {...attributes} {...listeners}>
 			<div
 				className={classes.priority}
 				onClick={handlePriorityChange}
@@ -126,10 +84,7 @@ const TaskCard: FC<ITaskCardProps> = ({ task, deleteTask, updateTask }) => {
 			<div className={classes.taskContent} onClick={() => setEditMode(true)}>
 				<p className={classes.taskContentWrapper}>{task.content}</p>
 			</div>
-			<button
-				className={classes.deleteTaskButton}
-				onClick={() => deleteTask(task.id)}
-			>
+			<button className={classes.deleteTaskButton} onClick={() => deleteTask(task.id)}>
 				<img src={deleteTaskButton} alt='delete this task' />
 			</button>
 		</div>
